b31726eb8f69a96e966186fdc3b037c69fd56936
Philipp Remy <philipp.remy@law-school.de>
2024-04-29 17:52:33 +0200

    Fix scrolling behavior in Editor.tsx

--- src/KampfgerichteRenderer.tsx
+++ src/KampfgerichteRenderer.tsx
@@ -1,6 +1,6 @@
 import KampfgerichtElement from "./Kampfgericht";
 import React from "react";
-import { FrontendStorage } from "./Editor";
+import { FrontendStorage, Kampfgericht } from "./Editor";
 import { Body2 } from "@fluentui/react-components";
 import { QuestionCircle32Filled } from "@fluentui/react-icons";
 
@@ -11,7 +11,26 @@ class KampfgerichteRenderer extends React.Component<{storage: FrontendStorage, s
     }
 
     createMap() {
-        return Array.from(this.props.storage.wk_judgingtables!, (val) => val[1]);
+        let arr = Array.from(this.props.storage.wk_judgingtables!, (val) => val[1]);
+        return this.sortArray(arr);
+    }
+
+    // The sorting function (callback)
+    sortArrayCallback(elem1: Kampfgericht, elem2: Kampfgericht): number {
+        let name1 = elem1.table_name.toUpperCase();
+        let name2 = elem2.table_name.toUpperCase();
+        if (name1 < name2) {
+            return -1;
+        }
+        if (name2 > name1) {
+            return 1;
+        }
+        return 0;
+    }
+
+    // Sort the array before passing it to the map() function
+    sortArray(arr: Kampfgericht[]) {
+        return arr.sort((elem1, elem2) => this.sortArrayCallback(elem1, elem2));
     }
 
     render() {
